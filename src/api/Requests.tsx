2d64265e61ec797a1f1eaa46011cf2c89937817e
NickGreenSF <nicholasgreensf@gmail.com>
2022-06-21 16:55:02 -0700

    routing initialized

--- src/api/Requests.tsx
+++ src/api/Requests.tsx
@@ -1,10 +1,16 @@
 import { apiHost } from '../constants/Api';
 import { deleteRequest, getRequest, postRequest } from './BaseRequests';
+import { GameProps } from '../types';
+
+type GetGamesReponse = {
+  games: GameProps[];
+};
 
 export const getAllGames = async () => {
   const url = `${apiHost}/games`;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const resp = await getRequest(url);
-  return resp;
+  return resp as GetGamesReponse;
 };
 
 export const samplePostGame = async (data: {
@@ -13,6 +19,7 @@ export const samplePostGame = async (data: {
   location?: string;
 }) => {
   const url = `${apiHost}/games`;
-  const resp = await postRequest(url, data);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const resp: GameProps = await postRequest(url, data);
   return resp;
 };
